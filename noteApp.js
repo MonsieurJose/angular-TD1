b986218d48090e12c7322ff86a8ae1f349caca2b
Clement_VIVIER <21505847@etu.unicaen.fr>
2017-01-24 16:05:09 +0100

    TD1 v3
    problème des fct rien ne s'affiche

--- noteApp.js
+++ noteApp.js
@@ -1,3 +1,5 @@
 /**
  * Created by cleme on 24/01/2017.
  */
+
+var NoteApp = angular.module("NoteApp",[]);
--- noteController.js
+++ noteController.js
@@ -1,3 +1,16 @@
 /**
  * Created by cleme on 24/01/2017.
  */
+
+NoteApp.controller('noteController',function(){
+    var self = this;
+    this.caractere = 100;
+    this.messageNote ="";
+    this.count = function () {
+        self.caractere = 100 - self.messageNote.length;
+    };
+    this.clear = function() {
+        self.messageNote = "";
+        self.caractere = 100;
+    };
+});
