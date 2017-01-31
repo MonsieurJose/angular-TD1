ecb2fc0f7adaecdb25d3f3094d2c20205e6ba21e
Clement_VIVIER <21505847@etu.unicaen.fr>
2017-01-31 15:59:53 +0100

    Fin de la partie 1 du TP1
    boostrap + ng-class

--- noteController.js
+++ noteController.js
@@ -5,18 +5,30 @@
 angular.module("NoteApp").controller("noteController",['$cookies',function($cookies){
     var self = this;
     this.messageNote =$cookies.get('msg');
-
+    var status = false;
+    var info = false;
+    var information = -1;
     this.count = function () {
         self.caractere = 100 - self.messageNote.length;
+        self.status = true;
+        self.info = false;
+        if (self.caractere>50){ self.information = 50; }
+        else if (self.caractere>20){ self.information = 20; }
+        else if (self.caractere>0){ self.information = 0; }
         return self.caractere;
     };
 
     this.clear = function() {
         self.messageNote = "";
         self.caractere = 100;
+        self.status = false;
+        self.info = false;
     };
     this.save = function() {
         $cookies.put('msg',self.messageNote);
+        self.info = true;
+        self.status = false;
+        self.information = 50;
     };
-    this.caractere = this.count();  
+    this.caractere = this.count();
 }]);
